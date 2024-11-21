6a5f0a2b61d77ed2d8a0c19ce05acaf66732a3be
Jules KJ <juljen2730@gmail.com>
2024-11-21 14:06:03 +0100

    smooth hover on ydelse

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let isScrolling;
 const logo = document.querySelector(".logo img");
 const header = document.querySelector("header");
 
+const ydelser = document.querySelector("a.ydelser");
+const ydelserImg = document.querySelector(".ydelser img");
+
 document.addEventListener("scroll", (e) => {
   window.clearTimeout(isScrolling);
 
   isScrolling = setTimeout(() => {
-    if (scrollY > 50) {
-      console.log("you are at the bottom");
-
+    if (scrollY > 100) {
       header.style.padding = "5px 0";
       logo.style.width = "80px";
       logo.style.transition = "0.5s";
